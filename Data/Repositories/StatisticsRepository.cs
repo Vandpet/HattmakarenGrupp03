9f64ee35bede25eb24ab07f2729c6ca1ece996d8
Vandpet <Vandpet@gmail.com>
2026-04-22 15:50:40 +0200

    Påbörjan av statistiken
    hej

---
+++ Data/Repositories/StatisticsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace HattmakarenWebbAppGrupp03.Data.Repositories
+{
+    public class StatisticsRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly HatOrderRepository _hatOrderRepository;
+
+        public StatisticsRepository(ApplicationDbContext db, HatOrderRepository hatOrderRepository)
+        {
+            _db = db;
+            _hatOrderRepository = hatOrderRepository;
+        }
+
+        public async Task<int> getAmoutTotalSoldHats()
+        {
+            var totalSoldHats = await _db.HatOrders
+                .Where(ho => ho.Status == "Skickad")
+                .ToListAsync();
+
+            var includeAllTotalSoldHats = 0;
+
+            foreach (var ho in totalSoldHats)
+            {
+                includeAllTotalSoldHats += ho.Amount;
+            }
+            return includeAllTotalSoldHats;
+        }
+
+        public async Task<int> getTotalRevenue()
+        {
+            var totalRevenue = await _db.HatOrders
+                .Where(ho => ho.Status == "Skickad")
+                .Include(ho => ho.Hat)
+                .ToListAsync();
+            int totalRevenueAmount = (int)totalRevenue.Sum(ho => ho.Hat.Price * ho.Amount);
+            return totalRevenueAmount;
+
+
+        }
+    }
+}
---
+++ Models/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HattmakarenWebbAppGrupp03.Models.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public int TotalSoldHats { get; set; }
+        public int TotalRevenue { get; set; }
+    }
+}
