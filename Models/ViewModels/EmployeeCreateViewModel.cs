9650fb342941c469db6fa2303c398f613e4027a2
Patrick Karlsson <patrick_karlsson@yahoo.se>
2026-04-28 14:38:46 +0200

    Småfix, se beskrivninng
    -När man skapar kund, ska den ej kolla dubletter på namn KLAR
    -telenr ska ha regex för telenr, samma för user KLAR
    -Navven ska ta en till customer index KLAR

--- Models/ViewModels/EmployeeCreateViewModel.cs
+++ Models/ViewModels/EmployeeCreateViewModel.cs
@@ -11,6 +11,7 @@ namespace HattmakarenWebbAppGrupp03.Models.ViewModels
         public string Adress { get; set; } = string.Empty;
 
         [Required]
+        [RegularExpression(@"^\+?[0-9\s\-]{7,15}$", ErrorMessage = "Invalid phone number")]
         public string PhoneNr { get; set; } = string.Empty;
 
         [Required]
